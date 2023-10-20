5bb77838a1e5385ee02c3df4ccf6d11389308917
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-20 09:27:15 +0200

    added sign in button in the navbar
    added btnType in a Custom Button Const and make it reusable

--- types/index.ts
+++ types/index.ts
@@ -6,6 +6,7 @@ export interface CustomButtonProps {
     title: string;
     containerStyles?: string;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
+    btnType?: "button" | "submit";
 
 
 }
