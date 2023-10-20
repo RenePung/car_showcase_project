6a5ee8b6ff17d4ab5f3dfd9f33ab4d8191e6ce65
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-20 11:11:43 +0200

    added links to h3 in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,6 +27,15 @@ const Footer = () => {
             <div key={link.title}
             className="footer__link">
               <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item) => (
+                <Link 
+                key={item.title}
+                href={item.url}
+                className="text-gray-500"
+                >
+                  {item.title}
+                </Link>
+              ))}
             </div>
           ))}
         </div>
