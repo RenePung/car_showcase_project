a389432cd49bb2b88159445320f63c8f92e4dd15
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-23 18:55:58 +0200

    added arrow icon when we hover over a car card

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -27,7 +27,10 @@ const CarCard = ({ car }: CarCardProps) => {
     = car;
 
 
-    const carRent = calculateCarRent(city_mpg, year);
+const [isOpen, setIsOpen] = useState(false);
+
+
+const carRent = calculateCarRent(city_mpg, year);
 
   return (
     <div className="car-card group">
@@ -77,6 +80,9 @@ const CarCard = ({ car }: CarCardProps) => {
                 <CustomButton 
                 title="View More"
                 containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+                textStyles="text-white text-[14px] leading-[17px] font-bold"
+                rightIcon="/right-arrow.svg"
+                handleClick={() => setIsOpen(true)}
                 />
             </div>
         </div>
