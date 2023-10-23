cf73303afe475a7d04170d6dcb843dab24d95f2d
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-23 11:35:37 +0200

    added combobox UI

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -57,10 +57,10 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                afterLeave={() => setQuery('')}
                >
 
-
+             
               <Combobox.Options>
                 {
-                  filteredManufacturers.map((item) => (
+                  filteredManufacturers.map((item) => ( // Combobox UI https://headlessui.com/react/combobox
                     <Combobox.Option
                     key={item}
                     className={({ active }) => `
@@ -69,7 +69,25 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                     `}
                     value={item}
                     >
-                      {item}
+                      {({ selected, active }) => (
+                        <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >
+                          </span>
+                        ) : null}
+                        </>
+                      )}
                     </Combobox.Option>
                   )
                 )}
