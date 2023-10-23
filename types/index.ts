caf72f2a6744810122df9cf39134cf068c17772b
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-23 16:23:23 +0200

    created carprops / car interface

--- types/index.ts
+++ types/index.ts
@@ -16,3 +16,19 @@ export interface SearchManufacturerProps {
     manufacturer: string;
     setManufacturer: (manufacturer: string) => void;
 }
+
+
+// instead of actual values ( number / text ) we specify the type of values ( number / string ).
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+}
