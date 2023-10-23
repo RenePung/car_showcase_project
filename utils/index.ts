9125c469f326a01fcda51daab53a37bc527d3908
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-23 12:25:35 +0200

    added explanation in a comment
    added explanation below the api fetch

--- utils/index.ts
+++ utils/index.ts
@@ -10,3 +10,27 @@ export async function fetchCars() {
 
         return result;
     }
+
+
+/*
+1.Function Declaration: This code defines an asynchronous function named fetchCars.
+The async keyword indicates that this function will return a promise.
+
+2.Request Headers: Inside the function, you define an object called headers which contains two headers for the API request. 
+These headers are used to authenticate and specify the target host for the API.
+
+3.API Request: The fetch function is used to make an HTTP request to the specified URL, which is the API endpoint: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars'. 
+It also includes the headers object in the request for authentication.
+
+4.Awaiting the Response: await is used to wait for the response from the API. 
+This is important in an asynchronous function because it ensures that the code will not continue until the response is received.
+
+5.Parsing the Response: Once the response is received, the response.json() method is used to parse the response data as JSON. 
+This is because API responses are usually in JSON format.
+
+6.Returning the Result: Finally, the parsed JSON data is returned as the result of the function.
+
+
+
+This function is used to fetch car data from the specified API and returns the data in a format that can be easily used in your application. 
+*/
