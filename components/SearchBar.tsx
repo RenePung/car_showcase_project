92dec2063d2052b3c00f1757c62e8560a740d3f2
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-24 17:01:43 +0200

    added image for filter search

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,9 +1,20 @@
 "use client";
+import Image from "next/image";
 import { SearchManufacturer } from ".";
 import { useState } from 'react';
 
 
-
+const SearchButton = ({ otherClasses }: { otherClasses: string}) => (
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image 
+    src="/magnifying-glass.svg"
+    alt="magnifying glass"
+    width={40}
+    height={40}
+    className="object-contain"
+    />
+  </button>
+)
 
 
 const SearchBar = () => {
@@ -20,6 +31,9 @@ const SearchBar = () => {
             manufacturer={manufacturer}
             setManufacturer={setManufacturer}
             />
+
+
+            <SearchButton otherClasses="sm:hidden"/>
         </div>
     </form>
   )
