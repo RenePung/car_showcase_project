d3779daa7e34794864a1db52fd5d6d460b29a744
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-24 08:53:42 +0200

    fixed typo added transitionj property

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -7,16 +7,36 @@ import Image from 'next/image';
 import { Dialog, Transition } from '@headlessui/react';
 
 
+
+
+
 interface CarDetailsProps {
     isOpen: boolean;
     closeModal: () => void;
     car: CarProps;
 }
 
+
+
+
+
+
 const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
   return (
-    <div>CarDetails</div>
+    <>
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Transition.Child>
+
+        </Transition.Child>
+      </Dialog>
+    </Transition>
+    </>
   )
 }
+
+
+
+
 
 export default CarDetails
