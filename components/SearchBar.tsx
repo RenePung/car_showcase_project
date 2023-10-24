03db111922efa8ba753c67e8e6ce0c3762ddd20e
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-24 18:44:14 +0200

    update search bar link change

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { SearchManufacturer } from ".";
 import { useState } from 'react';
-
+import { useRouter } from 'next/navigation';
 
 const SearchButton = ({ otherClasses }: { otherClasses: string}) => (
   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
@@ -22,16 +22,44 @@ const SearchBar = () => {
 
     const [model, setModel] = useState('');
 
+    const router = useRouter();
 
 
 
 
+/*Alert for the search bar*/
     const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
 
       if(manufacturer === '' && model === '') {
         return alert('Please Fill In The Search Bar')
       }
+
+      updateSearchParams(
+        model.toLowerCase(), 
+        manufacturer.toLowerCase()
+        )
+    }
+
+/*forming a new url for a search bar*/
+    const updateSearchParams = (model: string, manufacturer: string) => {
+      const searchParams = new URLSearchParams(window.location.search);
+
+      if(model) {
+        searchParams.set('model', model)
+      } else {
+        searchParams.delete('model')
+      }
+
+      if(manufacturer) {
+        searchParams.set('manufacturer', manufacturer)
+      } else {
+        searchParams.delete('manufacturer')
+      }
+
+      const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+
+      router.push(newPathname)
     }
 
 
