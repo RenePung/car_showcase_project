d52c80b42bea6b14db8141182797e93f9da25710
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-24 14:43:42 +0200

    fixed bugs & added styling

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -55,7 +55,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
         leaveFrom="opacity-100 scale-100"
         leaveTo="opacity-0 scale-95"
         >
-          <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xsl transition-all flex flex-col gap-5">
+          <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5">
             <button type="button" onClick={closeModal} className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full">
               <Image 
               src="/close.svg"
@@ -94,8 +94,8 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
               <div className="mt-3 flex flex-wrap gap-4">
                 {Object.entries(car).map(([key, value]) => (
                   <div className="flex justify-between gap-5 w-full text-right" key={key}>
-                    <h4>{key}</h4>
-                    <p>{value}</p>
+                    <h4 className="text-grey capitalize">{key.split("_").join(" ")}</h4>
+                    <p className="text-black-100 font-semibold">{value}</p>
                   </div>
                 ))}
               </div>
