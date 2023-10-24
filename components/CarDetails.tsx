62efb6ace2d076be5b8af34716cc3135c1a18d5c
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-24 10:45:11 +0200

    added button and styling

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -55,7 +55,21 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
         leaveFrom="opacity-100 scale-100"
         leaveTo="opacity-0 scale-95"
         >
+          <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xsl transition-all flex flex-col gap-5">
+            <button type="button" onClick={closeModal} className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full">
+              <Image 
+              src="/close.svg"
+              alt="close"
+              width={20}
+              height={20}
+              className="object-contain"
+              />
+            </button>
 
+            <div>
+              
+            </div>
+          </Dialog.Panel>
         </Transition.Child>
           </div>
         </div>
