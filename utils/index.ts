f58f8d9deaf3ced427d42e7faa118f50a7b9ea9e
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-25 09:23:16 +0200

    added search params for fetching

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,6 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
     const headers = {
             'X-RapidAPI-Key': '464bea8d7fmshe60525f3dbf268ep1a2abcjsn4aa5a344fa93',
             'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
