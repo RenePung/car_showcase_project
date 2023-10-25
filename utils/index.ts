ee3d5979009e664e5c738d1fd2169d1744b24fee
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-25 09:36:18 +0200

    search inpout bar is working
    we are fetching data !!

--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,16 @@
 import { CarProps, FilterProps } from "@/types";
 
 export async function fetchCars(filters: FilterProps) {
+    
+    const { manufacturer, year, model, limit, fuel } = filters;
+    
+
     const headers = {
             'X-RapidAPI-Key': '464bea8d7fmshe60525f3dbf268ep1a2abcjsn4aa5a344fa93',
             'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
         }
 
-        const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=ghost', {headers: headers,});
+        const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {headers: headers,});
 
         const result = await response.json();
 
