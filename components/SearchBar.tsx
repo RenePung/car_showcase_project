755d2fe57b848215e76de932d533d605d0f509dd
Rene Pungartnik <rene.pungartnik98@gmail.com>
2023-10-26 11:49:13 +0200

    fixed bug next.js scroll top
    added scroll:false to router.push(newpathname)

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -59,7 +59,7 @@ const SearchBar = () => {
 
       const newPathname = `${window.location.pathname}?${searchParams.toString()}`
 
-      router.push(newPathname)
+      router.push(newPathname, {scroll:false});
     }
 
 
